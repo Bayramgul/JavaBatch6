2eccd0dff933e1b7003ca4efebe715ccae34d370
Bayramgul <atageldiyevab@gmail.com>
2020-03-21 23:38:31 -0400

    Repl104 class added to Repl package

---
+++ src/Repl/Repl104.java
@@ -0,0 +1,22 @@
+package Repl;
+
+public class Repl104 {
+
+	public static void main(String[] args) {
+		/*
+		 * 1. create method and name as m1. Inside your method body add print statement
+		 * as "m1 method implementation" 2. create another method and name as m2. Inside
+		 * your method body add print statement as "m2 method implementation" 3. Call
+		 * these two methods in the main method
+		 */
+		 Repl104 obj=new Repl104();
+		  obj.m1();
+		  obj.m2();
+	}
+	void m1(){
+	    System.out.println("m1 method implementation");
+	  }void m2(){
+	    System.out.println("m2 method implementation");
+	  }
+
+}
