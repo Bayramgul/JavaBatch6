8e1a808139f423a6d493212d0fb547f13602c651
Bayramgul <atageldiyevab@gmail.com>
2020-03-22 10:32:36 -0400

    New package com.syntax.class15 is created

---
+++ src/Repl/Repl105.java
@@ -0,0 +1,20 @@
+package Repl;
+
+public class Repl105 {
+	int number;
+
+	void print() {
+		for (int number = 1; number <= 10; number++) {
+
+			System.out.println(number);
+		}
+	}
+
+	public static void main(String[] args) {
+		Repl105 obj = new Repl105();
+		obj.number = 1;
+		obj.print();
+
+	}
+
+}
